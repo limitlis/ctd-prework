c4ebe6804d6481c61c9ac7f6633a05681aff5915
Cesar Vargas <limitlis@gmail.com>
2026-07-06 22:19:20 -0700

    Add Tailwind, Xior and migrate to Vite+

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# ctd-prework
+# Code the Dream Advanced PreWork Assignment
 
 ## Work Plan
 
-### Research 
+### Research
 
 - Use the Art Institute of Chicago API for data, content, and images.
-  - Review the AIC api to find a common reusable pattern so that pages & components are generic and reusable 
+  - Review the AIC api to find a common reusable pattern so that pages & components are generic and reusable
 - Site-wide search functionality that includes result previews and keyboard controls
-
 
 ## Project Setup
 
+This project is using Vite+
+
 ```sh
-npm install
+vp install
 ```
 
 ### Compile and Hot-Reload for Development
 
 ```sh
-npm run dev
+vp dev
 ```
 
 ### Compile and Minify for Production
 
 ```sh
-npm run build
+vp build
 ```
 
-### Lint with [ESLint](https://eslint.org/)
+### Lint & Format
 
 ```sh
-npm run lint
+vp lint
+vp fmt
 ```
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,57 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite-plus'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    fmt: {
+        semi: false,
+        singleQuote: true,
     },
-  },
+    lint: {
+        plugins: ['eslint', 'unicorn', 'oxc', 'vue'],
+        categories: {
+            correctness: 'error',
+        },
+        env: {
+            browser: true,
+            builtin: true,
+        },
+        ignorePatterns: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+        rules: {
+            'no-case-declarations': 'error',
+            'no-empty': 'error',
+            'no-fallthrough': 'error',
+            'no-prototype-builtins': 'error',
+            'no-redeclare': 'error',
+            'no-regex-spaces': 'error',
+            'no-undef': 'error',
+            'no-unexpected-multiline': 'error',
+            'no-useless-assignment': 'error',
+            'preserve-caught-error': 'error',
+            'vite-plus/prefer-vite-plus-imports': 'error',
+        },
+        options: {
+            typeAware: true,
+            typeCheck: true,
+        },
+        jsPlugins: [
+            {
+                name: 'vite-plus',
+                specifier: 'vite-plus/oxlint-plugin',
+            },
+        ],
+    },
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    server: {
+        port: 5155,
+    },
 })
